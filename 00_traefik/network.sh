d238da73b272dae5ca4cd17357389219e928b736
root <root@srv456397.hstgr.cloud>
2025-09-15 15:55:44 +0000

    fixing large files

--- 00_traefik/network.sh
+++ 00_traefik/network.sh
@@ -1,11 +1,9 @@
 
-# docker network create --driver bridge --subnet=172.20.0.0/16 traefik
-
 docker network create \
-	--driver=bridge \
-	--subnet=172.20.0.0/16 \
-	--gateway=172.20.0.1 \
-	--ipv6 \
-	--subnet=2a02:4780:10:e8ad::/64 \
-	--gateway=2a02:4780:10:e8ad::1 \
-	traefik
+    --driver=bridge \
+    --subnet=10.10.0.0/16 \
+    --gateway=10.10.0.1 \
+    --ipv6 \
+    --subnet=fd12:3456:789a::/64 \
+    --gateway=fd12:3456:789a::1 \
+    npm_net
