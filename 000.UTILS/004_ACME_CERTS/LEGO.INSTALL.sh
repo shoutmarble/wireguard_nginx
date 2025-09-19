fc72fadbb3c03fe64fd86201020da90842ab28b4
Your Name <terry.walsworth@outlook.com>
2025-09-19 16:51:09 +0000

    readd

--- 000.UTILS/004_ACME_CERTS/LEGO.INSTALL.sh
+++
@@ -1,5 +0,0 @@
-wget -O lego.v4.tar.gz https://github.com/go-acme/lego/releases/download/v4.25.2/lego_v4.25.2_linux_amd64.tar.gz
-
-tar -xvzf lego.v4.tar.gz --one-top-level=lego.v4
-
-rm lego.v4.tar.gz
